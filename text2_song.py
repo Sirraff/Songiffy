067f314c0e24b95d7e88a3be369e144f54b7e1ac
Raf <rafael.ls.reis@gmail.com>
2023-04-17 15:19:41 -0700

    Create text2_song.py
    V 0.0 -- start of project

---
+++ text2_song.py
@@ -0,0 +1,10 @@
+"""
+ * Title: Text2Song
+ * Abstract:  text to music converter using sentiment analysis and music theory
+ * Authors:  Rafael L.S Reis, Nicholas Garcia, Jesus Jimenez
+ * Email: rafael.ls.reis@gmail.com 
+ * Start Date: 04/17/2023
+ * Using: 
+"""
+
+# Main code here #
