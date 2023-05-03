6cb9a8d7f9850e0f2268feb0181e36d7c962acd5
Daysi Perez <98241116+daysipg@users.noreply.github.com>
2023-05-03 00:34:04 -0700

    Update AlgoRythm.py
    added more randomness to the scale and pattern

--- AlgoRythm/AlgoRythm.py
+++ AlgoRythm/AlgoRythm.py
@@ -24,16 +24,21 @@ sentiment = TextBlob(text).sentiment.polarity
 
 print("Sentiment is :", sentiment)
 
-# Define the musical patterns based on sentiment
-if sentiment > 0.5:
-    scale = [0, 2, 4, 5, 7, 9, 11]  # major scale for positive sentiment
-    pattern = [0, 2, 4, 5, 4, 2]  # pattern for positive sentiment
-elif sentiment < -0.5:
-    scale = [0, 2, 3, 5, 7, 8, 10]  # minor scale for negative sentiment
-    pattern = [0, 3, 5, 3, 0, -2]  # pattern for negative sentiment
-else:
-    scale = [0, 2, 3, 5, 7, 9, 10]  # mixed scale for neutral sentiment
-    pattern = [0, 2, 0, 3, 0, 2]  # pattern for neutral sentiment
+def get_scale_and_pattern(sentiment):
+    if sentiment > 0.5:
+        scale = [0, 2, 4, 5, 7, 9, 11]  # major scale for positive sentiment
+        pattern = random.choices([0, 2, 4, 5, 4, 2], k=10)  #randomly chooses 10 notes from the major scale
+        #pattern = [0, 2, 4, 5, 4, 2]  # pattern for positive sentiment
+    elif sentiment < -0.5:
+        scale = [0, 2, 3, 5, 7, 8, 10]  # minor scale for negative sentiment
+        pattern = random.choices([0, 3, 5, 3, 0, -2], k=10)  #randomly chooses 10 notes from the minor scale
+    else:
+        scale = [0, 2, 3, 5, 7, 9, 10]  # mixed scale for neutral sentiment
+        pattern = random.choices([0, 2, 0, 3, 0, 2], k=10)  #randomly chooses 10 notes from the mixed scale
+    return scale, pattern
+
+# Get the scale and pattern based on sentiment
+scale, pattern = get_scale_and_pattern(sentiment)
 
 print("scale: ", scale)
 print("pattern: ", pattern)
