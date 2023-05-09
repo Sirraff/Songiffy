874335b6b668b0bb765c661f25ba0d5d97098a24
Daysi Perez <98241116+daysipg@users.noreply.github.com>
2023-05-08 20:38:06 -0700

    Update AlgoRythm.py

--- AlgoRythm/AlgoRythm.py
+++ AlgoRythm/AlgoRythm.py
@@ -4,6 +4,7 @@ from textblob import TextBlob
 import random
 import os
 import numpy as np
+import scipy.io.wavfile as wavfile
 
 # Define the mapping between letters and notes
 notes = {
@@ -22,23 +23,49 @@ text = input("Type text here: ")
 # Use TextBlob to determine the sentiment of the text
 sentiment = TextBlob(text).sentiment.polarity
 
-print("Sentiment is :", sentiment)
-
 def get_scale_and_pattern(sentiment):
-    if sentiment > 0.5:
-        scale = [0, 2, 4, 5, 7, 9, 11]  # major scale for positive sentiment
-        pattern = random.choices([0, 2, 4, 5, 4, 2], k=10)  #randomly chooses 10 notes from the major scale
-        #pattern = [0, 2, 4, 5, 4, 2]  # pattern for positive sentiment
-    elif sentiment < -0.5:
-        scale = [0, 2, 3, 5, 7, 8, 10]  # minor scale for negative sentiment
-        pattern = random.choices([0, 3, 5, 3, 0, -2], k=10)  #randomly chooses 10 notes from the minor scale
-    else:
+    if sentiment > 0.7:
+        scale = [0, 2, 4, 5, 7, 9, 11]  # major scale for very positive sentiment
+        pattern = random.choices([0, 2, 4, 5, 4, 2], k=10)  #randomly chooses 20 notes from the major scale
+    elif sentiment > 0.3:
+        scale = [0, 2, 4, 5, 7, 9, 10]  # mixed scale for positive sentiment
+        pattern = random.choices([0, 2, 4, 5, 4, 2], k=10)  #randomly chooses 20 notes from the mixed scale
+    elif sentiment > -0.3:
         scale = [0, 2, 3, 5, 7, 9, 10]  # mixed scale for neutral sentiment
-        pattern = random.choices([0, 2, 0, 3, 0, 2], k=10)  #randomly chooses 10 notes from the mixed scale
+        pattern = random.choices([0, 2, 0, 3, 0, 2], k=10)  #randomly chooses 20 notes from the mixed scale
+    elif sentiment > -0.7:
+        scale = [0, 2, 3, 5, 6, 8, 10]  # sad scale for negative sentiment
+        pattern = random.choices([0, -2, -3, -2, 0, -2], k=10)  #randomly chooses 20 notes from the sad scale
+    else:
+        scale = [0, 2, 3, 5, 7, 8, 10]  # minor scale for very negative sentiment
+        pattern = random.choices([0, -3, -5, -3, 0, 2], k=10)  #randomly chooses 20 notes from the angry scale 
     return scale, pattern
+
 
 # Get the scale and pattern based on sentiment
 scale, pattern = get_scale_and_pattern(sentiment)
 
+print("Sentiment is :", sentiment)
 print("scale: ", scale)
 print("pattern: ", pattern)
+
+#Audio data
+duration = 1.0  #Each note in seconds
+sampling_rate = 44100  #Sampling rate of the audio data
+amplitude = 4096  #Amplitude
+
+#Audio data for each note
+audio_data = []
+for note_index in pattern:
+    frequency = 440 * 2 ** (scale[note_index] / 12.0)  #frequency of the note
+    time_array = np.arange(0, duration, 1 / sampling_rate)  #time array
+    waveform = amplitude * np.sin(2 * np.pi * frequency * time_array)  #waveform
+    audio_data.append(waveform)
+
+#Concatenate the audio data for all the notes
+audio_data = np.concatenate(audio_data)
+
+#Save the audio data as a .wav file
+filename = "song.wav"
+wavfile.write(filename, sampling_rate, audio_data.astype(np.int16))
+print("File saved as", filename)
